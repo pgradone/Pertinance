2a342ec558aaaff2baf973fcf75a213c88edbe60
Potito Gradone <pgradone@gmail.com>
2023-04-11 18:30:22 +0200

    fix =docName in _form_fields, forEach

--- models/doc.js
+++ models/doc.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 
 const docSchema = new mongoose.Schema({
-  ID_DOC: {
-    type: Number,
-    required: false,
-  },
+  // ID_DOC: {
+  //   type: Number,
+  //   required: false,
+  // },
   docName: {
     type: String,
     required: true,
   },
-  docText: {
-    type: String,
-    required: false,
-  },
-  docElement: {
-    type: String,
-    required: false,
-  },
+  // docText: {
+  //   type: String,
+  //   required: false,
+  // },
+  // docElement: {
+  //   type: String,
+  //   required: false,
+  // },
 });
 
 module.exports = mongoose.model('Doc', docSchema);
