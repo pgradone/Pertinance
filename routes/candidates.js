ab3beaa485fc23a30996d57ad585b2ad81e8aa0f
Potito Gradone <pgradone@gmail.com>
2023-04-19 12:22:12 +0200

    add candidates route and views and index

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,37 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const Doc = require('../models/candidate');
+
+fetchCandidates();
 
 // All candidates route
-router.get('/', async (req, res) => {
-  let searchOptions = {};
-  if (req.query.id != null && req.query.id !== '') {
-    searchOptions.id = new req.query.id();
-  }
-  if (req.query.firstName != null && req.query.firstName !== '') {
-    searchOptions.firstName = new RegExp(req.query.firstName, 'i');
-  }
-  if (req.query.lastName != null && req.query.lastName !== '') {
-    searchOptions.lastName = new RegExp(req.query.lastName, 'i');
-  }
-  if (req.query.skills != null && req.query.skills !== '') {
-    searchOptions.skills = new RegExp(req.query.skills, 'i');
-  }
-  if (req.query.title != null && req.query.title !== '') {
-    searchOptions.title = new RegExp(req.query.title, 'i');
-  }
-  if (req.query.email1 != null && req.query.email1 !== '') {
-    searchOptions.email1 = new RegExp(req.query.email1, 'i');
-  }
-  try {
-    const candidates = await Candidate.find(searchOptions);
-    res.render('candidates/index', {
-      candidates: candidates,
-      searchOptions: req.query,
-    });
-  } catch {
-    res.redirect('/');
-  }
+router.get('/', (req, res) => {
+  res.render('candidates/index');
 });
+
+async function fetchCandidates() {
+  const authString = process.env.JFUser + ':' + process.env.JFAuth;
+  const options = {
+    headers: {
+      Authorization: 'Basic' + btoa(authString),
+    },
+  };
+  // const response = await fetch(process.env.boond_url & '/candidates', options);
+  // const data = await response.json();
+  // console.log(data);
+  console.log(authString);
+}
+// function renderFromBoond() {
+//   const apiUrl = process.env.boond_url & "/candidates"
+
+// fetch(apiUrl)
+// .then(express.response => express.response.json())
+// .catch(error => console.log(error))
+// }
 
 module.exports = router;
