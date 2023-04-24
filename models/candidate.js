93afe06c1f5ec918c1d7e89328c4d0a6622d0a2e
Potito Gradone <pgradone@gmail.com>
2023-04-24 10:52:05 +0200

    candidates model and routes, trial 4

--- models/candidate.js
+++ models/candidate.js
@@ -1,32 +1,32 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const btoa = require('btoa');
 
-// const candidateSchema = new mongoose.Schema({
-//   Candidate_ID: {
-//     type: Number,
-//     required: false,
-//   },
-//   firstName: {
-//     type: String,
-//     required: true,
-//   },
-//   lastName: {
-//     type: String,
-//     required: false,
-//   },
-//   skills: {
-//     type: String,
-//     required: false,
-//   },
-//   title: {
-//     type: String,
-//     required: false,
-//   },
-//   email1: {
-//     type: String,
-//     required: false,
-//   },
-// });
+const candidateSchema = new mongoose.Schema({
+  Candidate_ID: {
+    type: Number,
+    required: false,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  skills: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: false,
+  },
+  email1: {
+    type: String,
+    required: false,
+  },
+});
 
 const fetchCandidates = async () => {
   const apiUrl = process.env.boond_url + '/candidates';
@@ -39,39 +39,27 @@ const fetchCandidates = async () => {
   const response = await fetch(apiUrl, options);
   const candi = await response.json();
   const candidates = candi.data;
-  // console.log(candidates);
-  candidates.forEach((candidate) => {
-    console.log(
-      candidate.id +
-        ' ' +
-        candidate.attributes.firstName +
-        ' ' +
-        candidate.attributes.lastName +
-        ' ' +
-        candidate.attributes.skills +
-        ' ' +
-        candidate.attributes.title +
-        ' ' +
-        candidate.attributes.email1
-    );
-  });
-  // return candidates;
-  module.exports = candidates; // Export data directly
+  return candidates; // Return the array of candidate objects
 };
 
-fetchCandidates();
-// candidateSchema.statics.getCandidateData = async function () {
-//   const candidatesData = await fetchCandidates();
-//   const data = candidatesData.data.map((candidate) => ({
-//     Candidate_ID: candidate.id,
-//     firstName: candidate.attributes.firstName,
-//     lastName: candidate.attributes.lastName,
-//     skills: candidate.attributes.skills,
-//     title: candidate.attributes.title,
-//     email1: candidate.attributes.email1,
-//   }));
-//   return data;
-// };
+// Define a static method on the candidateSchema that maps the data to the schema
+//  and creates new objects in the database
+candidateSchema.statics.addCandidates = async function () {
+  try {
+    const candidatesData = await fetchCandidates();
+    const data = candidatesData.map((candidate) => ({
+      Candidate_ID: candidate.id,
+      firstName: candidate.attributes.firstName,
+      lastName: candidate.attributes.lastName,
+      skills: candidate.attributes.skills,
+      title: candidate.attributes.title,
+      email1: candidate.attributes.email1,
+    }));
+    await this.create(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-// module.exports = mongoose.model('Candidate', candidateSchema);
-// module.exports = candidates;
+module.exports = mongoose.model('Candidate', candidateSchema);
+// module.exports = fetchCandidates;
--- routes/candidates.js
+++ routes/candidates.js
@@ -12,15 +12,4 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const candidateData = await Candidate.getCandidateData();
-    await Candidate.create(candidateData);
-    res.redirect('/candidates');
-  } catch (err) {
-    console.error(err);
-    res.redirect('/');
-  }
-});
-
 module.exports = router;
