51fc40a756cbda48fbc0e5aeb3d7ee19139b4069
Potito Gradone <pgradone@gmail.com>
2023-05-03 14:55:18 +0200

    candidates model AI optimization

--- models/candidate.js
+++ models/candidate.js
@@ -2,7 +2,13 @@ const btoa = require('btoa');
 const Kwd = require('../models/kwd');
 
 class Candidate {
+  static candidates = null;
+
   static async fetchCandidates() {
+    if (this.candidates !== null) {
+      return this.candidates;
+    }
+
     const apiUrl = process.env.boond_url + '/candidates';
     const authString = btoa(process.env.JFUser + ':' + process.env.JFAuth);
     const options = {
@@ -11,28 +17,10 @@ class Candidate {
       },
     };
     const response = await fetch(apiUrl, options);
-    const candi = await response.json();
-    const candidates = candi.data;
-    return candidates;
+    const { data } = await response.json();
+    this.candidates = data;
+    return data;
   }
-
-  // static async addCandidates() {
-  //   try {
-  //     const candidatesData = await Candidate.fetchCandidates();
-  //     const data = candidatesData.map((candidate) => ({
-  //       Candidate_ID: candidate.id,
-  //       firstName: candidate.attributes.firstName,
-  //       lastName: candidate.attributes.lastName,
-  //       skills: candidate.attributes.skills,
-  //       title: candidate.attributes.title,
-  //       email1: candidate.attributes.email1,
-  //     }));
-  //     console.log(data);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
-
   static async findById(id) {
     const candidates = await Candidate.fetchCandidates();
     return candidates.find((c) => c.id === id) || null;
@@ -41,27 +29,15 @@ class Candidate {
   static async find(query) {
     const candidates = await Candidate.fetchCandidates();
     const keys = Object.keys(query);
-    return candidates.filter((candidate) => {
-      for (const key of keys) {
-        if (key !== 'id') {
-          if (
-            !candidate.attributes[key]
+    return candidates.filter((candidate) =>
+      keys.every((key) =>
+        key === 'id'
+          ? candidate[key].includes(query[key])
+          : candidate.attributes[key]
               .toLowerCase()
               .includes(query[key].toLowerCase())
-          ) {
-            return false;
-          }
-        } else {
-          if (
-            !candidate[key].includes(query[key])
-            // candidate[key] != query[key]
-          ) {
-            return false;
-          }
-        }
-      }
-      return true;
-    });
+      )
+    );
   }
 
   static async matchedKwds(id) {
